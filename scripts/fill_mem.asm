a9d9cdece654bd344a910dee14eea1f3899186d2
William Y. Feng <womogenes2@gmail.com>
2025-05-17 23:59:38 -0400

    Prevent fill_mem from writing its own code lol

--- scripts/fill_mem.asm
+++ scripts/fill_mem.asm
@@ -1,19 +1,26 @@
 .include _common.asm
 
-li a0, 0xfff0
 
 // a1 <- mask
 li a1, 0xf0f0
 
+start:
+  // Start address
+  li a0, 0xfff0
+
 loop:
+
   // Flip the bits
   lw a2, 0(a0)
-  // xor a2, a2, a1
-  addi a2, a2, 1
+  xor a2, a2, a1
 
   sw a2, 0(a0)
 
   addi a0, a0, 1
+
+  // Have we gone too far?
+  bz a0, start
+
   j loop
 
 halt
